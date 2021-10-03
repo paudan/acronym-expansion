08a1b7fb4cf70ffbfb968ab95968fffd9f55f200
paulius <danpaulius@gmail.com>
2021-10-04 02:39:49 +0300

    Update run scripts

--- run_pairwise.sh
+++ run_pairwise.sh
@@ -6,3 +6,12 @@ python3 features/pairwise_features.py --input-file data/test.json --output-file 
 
 python3 models/pairwise.py train-model --training-data dataset_train_bert_pairwise.h5 --validation-data dataset_dev_bert_pairwise.h5 --output-dir=bert-pairwise
 python3 models/pairwise.py predict-sentences --test-data data/test.json --model-dir bert-pairwise --transformer-model bert-base-uncased --embed-full
+
+# Train model for deployment
+python3 models/pairwise_full.py train-model \
+  --training-data data/train.json \
+  --validation-data data/dev.json \
+  --dictionary-file data/diction.json \
+  --transformer-model bert-base-uncased  \
+  --output-dir=bert-pairwise \
+  --batch-size=8
