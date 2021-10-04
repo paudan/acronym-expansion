aeccb6020499750be5f2bcaa1542ee4666b063e5
paulius <danpaulius@gmail.com>
2021-10-04 10:15:05 +0300

    Update run scripts

--- run_siamese.sh
+++ run_siamese.sh
@@ -5,7 +5,7 @@ python3 features/siamese_features.py --input-file data/train.json --output-file 
 python3 features/siamese_features.py --input-file data/dev.json --output-file dataset_dev_bert_siamese.h5 --embed-full \
   transformer-features --model-name bert-base-uncased
 python3 features/siamese_features.py --input-file data/test.json --output-file dataset_test_bert_siamese.h5 --embed-full \
-  transformer-features --model-name bert-base-uncased  --embed-full
+  transformer-features --model-name bert-base-uncased
 
 python3 models/siamese.py train-model --training-data dataset_train_bert_siamese.h5 --validation-data dataset_dev_bert_siamese.h5 --output-dir=bert-siamese
 python3 models/siamese.py predict-sentences --test-data data/test.json --model-dir bert-siamese --transformer-model bert-base-uncased --embed-full
